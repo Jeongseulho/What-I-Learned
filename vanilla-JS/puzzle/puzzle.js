2fdbe9742c10bacf435c67955fcaeb02111a5707
Jeongseulho <wjdtmfgh@yu.ac.kr>
2022-08-27 15:18:04 +0900

    drag event

---
+++ vanilla-JS/puzzle/puzzle.js
@@ -0,0 +1,56 @@
+const container = document.querySelector(".image-container");
+const startButton = document.querySelector(".start-button");
+const gameText = document.querySelector(".game-text");
+const playTime = document.querySelector(".play-time");
+
+const tileCount = 16;
+
+let tiles = [];
+
+setGame();
+
+function setGame() {
+  container.innerHTML = "";
+  tiles = createImageTiles();
+  tiles.forEach((tile) => container.appendChild(tile));
+  setTimeout(() => {
+    container.innerHTML = "";
+    shuffle(tiles).forEach((tile) => container.appendChild(tile));
+  }, 2000);
+}
+
+function createImageTiles() {
+  const tempArray = [];
+  Array(tileCount)
+    .fill()
+    .forEach((_, i) => {
+      const li = document.createElement("li");
+      li.setAttribute("data-index", i);
+      li.setAttribute("draggable", "true");
+      li.classList.add(`list${i}`);
+      tempArray.push(li);
+    });
+  return tempArray;
+}
+
+function shuffle(array) {
+  let index = array.length - 1;
+  while (index > 0) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+    index--;
+  }
+  return array;
+}
+
+container.addEventListener("dragstart", (e) => {
+  console.log("start");
+});
+
+container.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+
+container.addEventListener("drop", (e) => {
+  console.log("drop");
+});
