29f9b5bd07c8df8b59aa5d414ce6b126cf820db9
Jeongseulho <wjdtmfgh@yu.ac.kr>
2022-08-28 22:36:13 +0900

    tetris,BLOCKS

--- vanilla-JS/tetris/tetris.js
+++ vanilla-JS/tetris/tetris.js
@@ -1,10 +1,36 @@
 const playground = document.querySelector(".playground > ul");
-console.log(playground);
 
-for (let i = 0; i < 20; i++) {
+const GAME_ROWS = 20;
+const GAEM_COLS = 10;
+
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+
+const BLOCKS = {
+  tree: [],
+};
+
+const movingItem = {
+  type: "",
+  direction: 0,
+  top: 0,
+  letf: 0,
+};
+
+init();
+
+function init() {
+  for (let i = 0; i < GAME_ROWS; i++) {
+    prependNewLine();
+  }
+}
+
+function prependNewLine() {
   const li = document.createElement("li");
   const ul = document.createElement("ul");
-  for (let j = 0; j < 10; j++) {
+  for (let j = 0; j < GAME_ROWS; j++) {
     const matrix = document.createElement("li");
     ul.prepend(matrix);
   }
